59d76042b660e8f7e1b159467fd5173b45fd0a9f
kmsed <80398736+kmsed@users.noreply.github.com>
2021-08-15 18:20:06 +0900

    initial

--- src/src/App.js
+++ src/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from "styled-components";
+import MbtiStart from './pages/MbtiStart'
+import img from './travel.jpg'
 
-function App() {
+const BackStyle =  styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${img});
+  background-size: cover;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BackStyle>
+        <MbtiStart/>
+      </BackStyle>
   );
 }
 
